9e70a482b8598658f8d4072345684e7e1657d07d
Roger <rogerchiu4@gmail.com>
2022-03-11 19:37:45 -0600

    test:matalab matrix

--- speckleImg.py
+++ speckleImg.py
@@ -1,5 +1,7 @@
+#! /usr/bin/python3
 import cv2
 import numpy as np
+from scipy.io import savemat
 
 rawSpeckleImg = cv2.imread('./speckleraw.tif',0)
 print(rawSpeckleImg.shape)
@@ -14,18 +16,19 @@ speckleImg = np.zeros(shape=(imgShape[0],imgShape[1]))
 windowSize = 5
 
 
-for i in range(0,totalRows):
-    for j in range(0,totalCols):
+for i in range(0,totalRows-5):
+    for j in range(0,totalCols-5):
         startRowIndex,startColIndex = i,j
         lastRowIndex = startRowIndex+windowSize
         lastColIndex = startColIndex+windowSize
         neighboursMat = rawSpeckleImg[startRowIndex:lastRowIndex,startColIndex:lastColIndex]
         stdDeviation = neighboursMat.std()
         mean = neighboursMat.mean()
-        speckleImg[i][j] = stdDeviation/mean
+        speckleImg[i+2][j+2] = stdDeviation/mean
 
 
-
+mdic = {"a": speckleImg}
+savemat("a.mat", mdic)
 cv2.imshow('raw speckle',rawSpeckleImg)
 cv2.imshow('speckle img',speckleImg)
 cv2.waitKey()
